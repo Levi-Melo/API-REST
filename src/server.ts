1a12a9b4e679db8d93556dc66f59fb72efce59c7
levi melo <levicontat@outlook.com>
2021-05-30 12:16:19 -0300

    trying take out node modules

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { dbConnection } from "./data/dbConnection";
 const dbSync = async () => {
   try {
     const result = await dbConnection.sync();
-    console.log("Sync" + result);
   } catch (error) {
     console.log(error);
   }
